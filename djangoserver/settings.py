17fab5b21378dd3c8bf5d1e91aa9698e50cfac98
richard <richardoyelabi@gmail.com>
2022-10-20 10:41:17 +0100

    sign in with email with allauth's help

--- djangoserver/settings.py
+++ djangoserver/settings.py
@@ -188,4 +188,17 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 SITE_ID = 1
 
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_USERNAME_REQUIRED = False
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 STREAM_DEFAULT_PERMISSION_CLASSES = (IsAuthenticated,)
