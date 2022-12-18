6301b192bc6f3e5be5f6572866485000fb3abaf7
richard <richardoyelabi@gmail.com>
2022-12-18 23:26:28 +0100

    added trailing slash to verification/id-upload

--- verifications/urls.py
+++ verifications/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import IdUploadView
 
 urlpatterns = [
-    path("id-upload", IdUploadView.as_view(), name="id_upload"),
+    path("id-upload/", IdUploadView.as_view(), name="id_upload"),
 ]
